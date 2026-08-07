1e32c12514c366dd4e1217ad20c2234aaa952d6b
hugo-21-94 <hugo.joos@outlook.com>
2026-08-07 17:04:22 +0200

    Configuration Render

--- app.py
+++ app.py
@@ -555,6 +555,6 @@ def error_500(error):
 if __name__ == "__main__":
     app.run(
         debug=True,
-        host="127.0.0.1",
-        port=8000
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8000))
     )
